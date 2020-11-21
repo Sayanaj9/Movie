04834f0d8f844650c0de24c6b6e6405cb581d11f
Sayana <sayana.joy@attinadsoftware.com>
2020-11-22 00:39:42 +0530

    Updates

---
+++ precache-manifest.c1bb2c3d57a00b4b8d7b1555a77fd204.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9789944b61f1b249ec756749fcc4636",
+    "url": "/MovieApp/index.html"
+  },
+  {
+    "revision": "d975ddcca6d77cffe756",
+    "url": "/MovieApp/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "660ee2482849ce194f71",
+    "url": "/MovieApp/static/css/main.b873f26e.chunk.css"
+  },
+  {
+    "revision": "d975ddcca6d77cffe756",
+    "url": "/MovieApp/static/js/2.913e2982.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/MovieApp/static/js/2.913e2982.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "660ee2482849ce194f71",
+    "url": "/MovieApp/static/js/main.0dfedc85.chunk.js"
+  },
+  {
+    "revision": "170ce17e66ab04893701",
+    "url": "/MovieApp/static/js/runtime-main.dfea68a4.js"
+  },
+  {
+    "revision": "ed1d014bf2b8b72f27d6af65c69f710a",
+    "url": "/MovieApp/static/media/TitilliumWeb-Regular.ed1d014b.ttf"
+  },
+  {
+    "revision": "d5f2a7f493e94ed267d41620eef143d9",
+    "url": "/MovieApp/static/media/poster1.d5f2a7f4.jpg"
+  },
+  {
+    "revision": "81ccf144677486906fcf751450652895",
+    "url": "/MovieApp/static/media/poster2.81ccf144.jpg"
+  },
+  {
+    "revision": "e242f0372e7bdbf2e48aa5afefc72b97",
+    "url": "/MovieApp/static/media/poster3.e242f037.jpg"
+  },
+  {
+    "revision": "56f097245c605a5d312f103dedb21bcb",
+    "url": "/MovieApp/static/media/poster4.56f09724.jpg"
+  },
+  {
+    "revision": "5ef7f79461c7a8c0a95652b813fb4722",
+    "url": "/MovieApp/static/media/poster5.5ef7f794.jpg"
+  },
+  {
+    "revision": "164b05a3cd770650dd30bc71f5407e99",
+    "url": "/MovieApp/static/media/poster6.164b05a3.jpg"
+  },
+  {
+    "revision": "23ac494a92660204d6b8acbba20aa0e9",
+    "url": "/MovieApp/static/media/poster7.23ac494a.jpg"
+  },
+  {
+    "revision": "ef89f612f6e3d684892d6525b7957ce4",
+    "url": "/MovieApp/static/media/poster8.ef89f612.jpg"
+  },
+  {
+    "revision": "65d6d5002a2f98860f29d3c61599c5ac",
+    "url": "/MovieApp/static/media/poster9.65d6d500.jpg"
+  }
+]);
